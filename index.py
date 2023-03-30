bf5faa6c82e9393ea155b83bfd74318e24655d71
wrhbra2013 <wrhbra2013@gmail.com>
2023-03-29 21:52:30 -0300

    29032023

--- index.py
+++ index.py
@@ -14,6 +14,7 @@ sql = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
 #Pagina Boas Vindas
 @app.route("/",methods=('GET','POST'))
 def boas_vindas():
+        
      if request.method =='POST':
      
         #Recebe dados
@@ -21,18 +22,51 @@ def boas_vindas():
         user = request.form['nome']
         ano = request.form['serie']
 
-        #Pesquisar no Banco de Dados
-        pesquisa = sql.execute(f"SELECT inst, nome, serie FROM estudante where inst={escola} and nome={user} and serie={ano};")
-        print(pesquisa)
-        resultado = pesquisa.fetchall()
-        
-        if resultado == None:
-            flash('Usuario NÃO Encontrado. \n Digite novamente! \n Ou Cadastre-se!')
-        else:
-            return redirect(url_for('home'))
+        #Tratando erros
+        try:
+            #Pesquisar no Banco de Dados
+            pesquisa = sql.execute(f"SELECT inst, nome, serie FROM estudante where inst='{escola}' and nome='{user}' and serie='{ano}';")
+            print(pesquisa)
+            resultado = pesquisa.fetchone()
 
+            if resultado == None:
+                flash('Usuario NÃO Encontrado. Digite novamente! Ou Cadastre-se!')
+                         
+
+        except Exception:
+            flash('Campos Vazios ou erro de digitação. Por favor digite novamente!')
+            
+        
      
-     return render_template('index.html')     
+     return render_template('index.html')
+
+@app.route("/index",methods=('GET','POST'))
+def index():
+     if request.method == 'GET':
+         flash('Entre com suas credenciais.')
+     
+     if request.method =='POST':
+     
+        #Recebe dados
+        escola = request.form['inst']
+        user = request.form['nome']
+        ano = request.form['serie']
+
+        #Tratando erros
+        try:
+            #Pesquisar no Banco de Dados
+            pesquisa = sql.execute(f"SELECT inst, nome, serie FROM estudante where inst='{escola}' and nome='{user}' and serie='{ano}';")
+            print(pesquisa)
+            resultado = pesquisa.fetchone()
+
+            if resultado == None:
+                flash('Usuario NÃO Encontrado. Digite novamente! Ou Cadastre-se!')
+                         
+
+        except Exception:
+            flash('Campos Vazios ou erro de digitação. Por favor digite novamente!')       
+     
+     return render_template('index.html')
 
 
       
@@ -45,19 +79,33 @@ def cadastro():
         escola = request.form['inst']
         user = request.form['nome']
         ano = request.form['serie']
-
-        #Pesquisar no Banco de Dados
-        sql.execute(f"INSERT INTO estudante(inst, nome, serie) VALUES ({escola},{user},{ano});")
-                
-        flash(f"Usuario {user} REGISTRADO com sucesso.!!")   
-  
+        
+        try:
+            #Pesquisar no Banco de Dados
+            sql.execute(f"INSERT INTO estudante(inst, nome, serie) VALUES ('{escola}','{user}','{ano}');")
+                  
+            flash(f"Usuario {user} REGISTRADO com sucesso.!!")
+        except Exception:
+            flash('Campos Vazios Ou erro de digitação. Favor digite novamente.')
+          
     return render_template('cadastro.html')
 
 @app.route("/home",methods=('GET','POST'))
 def home():
+    
     flash('Bem Vindo' )   
   
     return render_template('home.html')
+
+@app.route("/gabarito")
+def gabarito():
+    return render_template('gabarito.html')
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+
 
 
 
